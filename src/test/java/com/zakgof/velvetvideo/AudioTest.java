2d7458bb553cd738c59ee6b4e6f9cc5e61d403f2
Alexander Zakusylo <zakgof@gmail.com>
2019-12-16 12:16:14 +0200

    Code cleanup

--- src/test/java/com/zakgof/velvetvideo/AudioTest.java
+++ src/test/java/com/zakgof/velvetvideo/AudioTest.java
@@ -21,10 +21,10 @@ public class AudioTest extends VelvetVideoTest {
          "matroska,       libvorbis",
          "mp4,            libvorbis",
          "webm,           libvorbis",
-//         "ogg,            vorbis",    // TODO Bad sound
-//         "webm,           vorbis",    // TODO Bad sound
-//         "ogg,            opus",      // TODO Bad sound
-//         "mp4,            opus",      // TODO Bad sound
+//         "ogg,            vorbis",    // Bad sound
+//         "webm,           vorbis",    // Bad sound
+//         "ogg,            opus",      // Bad sound
+//         "mp4,            opus",      // Bad sound
          "matroska,       aac",       // Warning: large mismatch
          "mp4,            aac",
          "flac,           flac",
@@ -38,17 +38,13 @@ public class AudioTest extends VelvetVideoTest {
          "mp4,            libopus",
     })
 	public void testAudioRecodeTest(String format, String codec) throws Exception {
-    //	File src = local("http://www.ee.columbia.edu/~dpwe/sounds/musp/msmn1.wav", "msmn1.wav");
-    //	File src = local("https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav", "cantina.wav");
     	File src = local("https://www.kozco.com/tech/piano2.wav", "piano2.wav");
-
 	    int duration = 6306;
-	    // int duration = 1675;
 		File dest = file("recode-"+ codec +"." + format);
 		System.err.println(dest);
 
 		byte[] bufferorig = readAudio(src, duration);
-		IVelvetVideoLib lib = new VelvetVideoLib();
+		IVelvetVideoLib lib = VelvetVideoLib.getInstance();
 		AudioFormat audioFormat = new AudioFormat(Encoding.PCM_SIGNED, 48000, 16, 2, 4, 48000, false);
 		try (IMuxer muxer = lib.muxer(format).audioEncoder(lib.audioEncoder(codec, audioFormat).enableExperimental()).build(dest)) {
 			IEncoderAudioStream encoder = muxer.audioEncoder(0);
@@ -58,10 +54,5 @@ public class AudioTest extends VelvetVideoTest {
 		}
 		byte[] bufferrecoded = readAudio(dest, duration);
 		assertAudioEqual(audioFormat, bufferorig, bufferrecoded);
-
 	}
-
-
-
-
 }
